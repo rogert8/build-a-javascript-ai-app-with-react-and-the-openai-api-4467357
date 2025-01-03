fbc7859c5e2e89f682245eebe79883d88c19d645
rogert8 <68079928+rogert8@users.noreply.github.com>
2025-01-03 07:07:56 +0000

    05_02b - OpenAI API call

--- src/components/PromptToLocation.jsx
+++ src/components/PromptToLocation.jsx
@@ -1,5 +1,21 @@
 import PropTypes from "prop-types";
 
+const url = "https://api.openai.com/v1/chat/completions";
+
+const data = {
+  model: "gpt-3.5-turbo-0125",
+  messages: [{ role: "user", content: prompt }],
+};
+
+const params = {
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_OPENAI}`,
+    "Content-type": "application/json",
+  },
+  body: JSON.stringify(data),
+  method: "POST",
+};
+
 const PromptToLocation = (prompt) => {
   return fetch(url, params)
     .then((response) => response.json())
