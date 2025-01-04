925ada14f16ab237f0a7d7f034396fa1ab6868b0
rogert8 <68079928+rogert8@users.noreply.github.com>
2025-01-04 06:53:51 +0000

    to weather api

--- src/components/PromptToLocation.jsx
+++ src/components/PromptToLocation.jsx
@@ -76,7 +76,14 @@ const PromptToLocation = (prompt) => {
           return `${promptRes.city},${promptRes.country}`;
         }
       };
-      console.log(locationString());
+      const promptData = {
+        locationString: locationString(),
+        units: promptRes.unit,
+        country: promptRes.country,
+        USstate: promptRes.USstate,
+      };
+      // console.log(locationString());
+      return promptData
     })
     .catch((error) => {
       console.log("Error:", error);
